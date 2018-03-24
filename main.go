3ae05fd6360ce5630a8fedf2b23f8b22a7e26bbc
Chris Jordan <chris.jordan@news.co.uk>
2018-03-24 17:15:24 +0000

    Adds proof of work.

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"fmt"
+	"math/big"
+	"math"
 )
 
 type Block struct {
@@ -13,10 +15,70 @@ type Block struct {
 	Data         []byte
 	PreviousHash []byte
 	Hash         []byte
+	Nonce        int
 }
 
 type BlockChain struct {
 	blocks []*Block
+}
+
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
+const targetBits = 24
+
+func NewProofOfWork(block *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+
+	return &ProofOfWork{block, target}
+}
+
+func (proofOfWork *ProofOfWork) prepareData(nonce int) []byte {
+	block := proofOfWork.block
+	return bytes.Join([][]byte{block.PreviousHash,
+		block.Data,
+		IntToHex(block.Timestamp),
+		IntToHex(int64(targetBits)),
+		IntToHex(int64(nonce))}, []byte{})
+}
+
+func (proofOfWork *ProofOfWork) Validate() bool {
+	var hashInt big.Int
+	data := proofOfWork.prepareData(proofOfWork.block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+	return hashInt.Cmp(proofOfWork.target) == -1
+}
+
+func (proofOfWork *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+	maxNonce := math.MaxInt64
+	fmt.Printf("Executing proof of work for %s\n", proofOfWork.block.Data)
+
+	for nonce < maxNonce {
+		data := proofOfWork.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(proofOfWork.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Printf("Hash %x\n", hash)
+	fmt.Printf("End of proof of work\n")
+	return nonce, hash[:]
+
+}
+
+func IntToHex(i int64) []byte {
+	return []byte(strconv.FormatInt(i, 16))
 }
 
 func (blockChain *BlockChain) AddBlock(data string) {
@@ -33,8 +95,12 @@ func (b *Block) setHash() {
 }
 
 func NewBlock(data string, PreviousHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), PreviousHash, []byte{}}
-	block.setHash()
+	block := &Block{time.Now().Unix(), []byte(data), PreviousHash, []byte{}, 0}
+	proofOfWork := NewProofOfWork(block)
+	nonce, hash := proofOfWork.Run()
+	block.Hash = hash
+	block.Nonce = nonce
+	fmt.Printf("PoW: %s\n", strconv.FormatBool(proofOfWork.Validate()))
 
 	return block
 }
